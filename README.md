7e87f0d2d655c17ce6c0e097a73b5cef410aa592
Ross Mawhorter <rmawhorter@g.hmc.edu>
2019-09-27 23:25:48 -0700

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # zebbo_quest
 Super Metroid ROMhack and Notes
+
+This a hack (in progress) where you play as a Zebbo (and Zebbo's friends: Zoa, Zeb, Gamet, Geega, and Fireflea)
+
+Someone has stolen your eggs, and it's your job to find them! Explore Zebes, and enlist the help of your friends to help you find your eggs.
+
+As a tiny insect, the environment of Zebes can be scary. With no way to defend yourself, you must rely on your superior speed and mobility. 
---
+++ you_are_a_zoa.asm
@@ -0,0 +1,119 @@
+LoRom
+
+; Top Half
+
+org $92D613 ;00
+DB #$00, #$E8, #$AF ; 0xAF:E800
+DW #$0200, #$0200
+
+org $92D62F ;04
+DB #$00, #$E8, #$AF
+DW #$0200, #$0200
+
+org $92D61A ;01
+DB #$00, #$E8, #$AF
+DW #$0200, #$0200
+
+org $92D636 ;05
+DB #$00, #$E8, #$AF
+DW #$0200, #$0200
+
+org $92D621 ;02
+DB #$00, #$E8, #$AF
+DW #$0200, #$0200
+
+org $92D63D ;06
+DB #$00, #$E8, #$AF
+DW #$0200, #$0200
+
+org $92D628 ;03
+DB #$00, #$E8, #$AF
+DW #$0200, #$0200
+
+org $92D644 ;07
+DB #$00, #$E8, #$AF
+DW #$0200, #$0200
+
+;;
+
+org $92CBEE ;00
+DB #$00, #$E8, #$AF
+DW #$0200, #$0200
+
+org $92CCD5 ;01
+DB #$00, #$E8, #$AF
+DW #$0200, #$0200
+
+; Replace Gravity suit palette with Zoa palette
+org $9B9800
+DW #$3800, #$72FA, #$55B0, #$2845, #$1801, #$6210, #$496B, #$38C6, #$2C63, #$1D79, #$1933, #$150E, #$10C8, #$3BE0, #$2680, #$1580
+
+; Replace spritemaps:
+; 1
+org $92BAB3
+DW #$0001
+
+DW #$C3F8 ; it's a 2x2 sprite
+DB #$F8
+DW #$2800
+
+;DW #$81F8
+;DB #$F8
+;DW #$2100
+
+; 2
+org $92BAE3
+DW #$0001
+
+DW #$C3F8
+DB #$F8
+DW #$2802
+
+; 3
+org $92BABF
+DW #$0001
+
+DW #$C3F8
+DB #$F8
+DW #$2804
+
+; 4
+org $92BAFE
+DW #$0001
+
+DW #$C3F8
+DB #$F8
+DW #$2806
+
+; 5
+org $92BACB
+DW #$0001
+
+DW #$C3F8
+DB #$F8
+DW #$2800
+
+; 6
+org $92BB0A
+DW #$0001
+
+DW #$C3F8
+DB #$F8
+DW #$2802
+
+; 7
+org $92BAD7
+DW #$0001
+
+DW #$C3F8
+DB #$F8
+DW #$2804
+
+; 8
+org $92BB16
+DW #$0001
+
+DW #$C3F8
+DB #$F8
+DW #$2806
+
